6e5ccbd22fb251d7ed9210346dd6c9d8173ec0ea
dalivat23 <dalivat23@gmail.com>
2026-09-20 21:57:35 +0000

    Added prompts for added COUT and CIN lines

--- main.cpp
+++ main.cpp
@@ -4,15 +4,35 @@
 using std::cout;
 using std::cin;
 using std::string;
+// To make coding lines easier to type out/read
+
 
 // Project 1 — Tristan Daliva
 // CIS 5 Week 04 · Student card
 
 int main() {
   const int CURRENT_YEAR = 2026;
+  string firstn;
+  int age;
+  int credits;
+  double gpa;
 
+  cout << "What is your first name? ";
+  cin >> firstn;
+  
+  cout << "What is your current age? ";
+  cin >> age;
 
+  cout << "How many credits are you taking this term? ";
+  cin >> credits;
 
+  cout << "What is your current GPA? ";
+  cin >> gpa;
+
+  
+  
+  
+  cout << "tests " << firstn << " " << age << " " << credits << " " << gpa << " ";
   // TODO (week 3): declare and initialize the four boxes
   //   first (std::string), age (int), credits (int), gpa (double)
 
